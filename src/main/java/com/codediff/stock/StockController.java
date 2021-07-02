90adc61e7172deff2763b213956a1b8d81396fe0
Lucas <lwmoehlenbrock@ucdavis.edu>
2021-07-02 10:57:21 -0500

    Added mappings to StockController

--- src/main/java/com/codediff/stock/StockController.java
+++ src/main/java/com/codediff/stock/StockController.java
@@ -1,11 +1,98 @@
 package com.codediff.stock;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class StockController {
 
     @Autowired
-    StockRepo repository;
+    UserRepo users;
+
+    @Autowired
+    User currentUser;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> newUser(@RequestBody User user){
+        if(users.getUsernames().contains(user.getUserName())){
+            return new ResponseEntity<String>("User " + user.getUserName() + " already exists", HttpStatus.IM_USED);
+        }
+        users.addUser(user);
+        return new ResponseEntity<String>("Welcome " + user.getUserName() + "!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user, HttpServletResponse response) throws IOException {
+        if(users.getIds().contains(user.getId())){
+            if(users.getUserById(user.getUserName()).getPassword() == user.getPassword()){
+                currentUser = users.getUserByID(user.getUserName());
+                response.sendRedirect("/account");
+                return new ResponseEntity<String>("Login Successful!", HttpStatus.ACCEPTED);
+            }
+            else{
+                return new ResponseEntity<String>("Wrong password, try again.", HttpStatus.UNAUTHORIZED);
+            }
+        }
+        else{
+            return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/account")
+    public User getCurrentUser(){
+        return currentUser;
+    }
+
+    @GetMapping("/stock/{stock}")
+    public String getStockPrice(@PathVariable String ticker){
+        return ticker + ": $" + StockRetriever.getMarketPrice(ticker);
+    }
+
+    @PostMapping("/deposit")
+    public double depositCash(@RequestBody double amount, HttpServletResponse response) throws IOException {
+        currentUser.setCash(currentUser.getCash() + amount);
+        response.sendRedirect("/account");
+        return currentUser.getCash();
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<String> withdrawCash(@RequestBody double amount, HttpServletResponse response) throws IOException {
+        if(currentUser.getCash() < amount){
+            return new ResponseEntity<String>("Insufficient funds", HttpStatus.NOT_ACCEPTABLE);
+        }
+        currentUser.setCash(currentUser.getCash() - amount);
+        response.sendRedirect("/account");
+        return new ResponseEntity<String>("Success! New balance: " + currentUser.getCash(), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/buy")
+    public ResponseEntity<String> buyStock(@RequestBody String ticker, @RequestBody Integer amount, HttpServletResponse response) throws IOException{
+        double marketPrice = StockRetriever.getMarketPrice(ticker);
+        if(marketPrice * amount > currentUser.getCash()){
+            return new ResponseEntity<String>("Insufficient funds", HttpStatus.NOT_ACCEPTABLE);
+        }
+        currentUser.getStockRepo().buyStock(ticker, amount);
+        response.sendRedirect("/account");
+        return new ResponseEntity<String>("Success!", HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/sell")
+    public ResponseEntity<String> sellStock(@RequestBody String ticker, @RequestBody Integer amount, HttpServletResponse response) throws IOException{
+        if(currentUser.getStockRepo().getStocks().get(ticker) < amount || !currentUser.getStockRepo().getStocks().containsKey(ticker)){
+            return new ResponseEntity<String>("You do not own enough of " + ticker, HttpStatus.NOT_ACCEPTABLE);
+        }
+        double marketPrice = StockRetriever.getMarketPrice(ticker);
+        currentUser.setCash(currentUser.getCash() + marketPrice * amount);
+        currentUser.getStockRepo().sellStock(ticker, amount);
+        response.sendRedirect("/account");
+        return new ResponseEntity<String>("Success! New balance: " + currentUser.getCash(), HttpStatus.ACCEPTED);
+
+    }
+
+
 }
