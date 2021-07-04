35de87259125101e66f67fc9aa6c763f9aa81c99
caspari-svg <holguinsebastian9@gmail.com>
2021-07-04 19:24:45 -0400

    Finalized StockRetriever to successfully connect to RapidAPI and retrieve a price as a double. Also added Gson dependency to xml file for Json readability.

--- src/main/java/com/codediff/stock/StockRetriever.java
+++ src/main/java/com/codediff/stock/StockRetriever.java
@@ -1,36 +1,26 @@
 package com.codediff.stock;
 
-import com.fasterxml.jackson.core.JsonParser;
+import com.google.gson.*;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class StockRetriever {
-
-    public static Response getCurrentResponse(String ticker) throws IOException {
-        String key = "8d1894121cmsh1e839afb82a7fcdp196250jsn3f32627c72f4";
-        String host = "apidojo-yahoo-finance-v1.p.rapidapi.com";
-
+    public static double getCurrentResponse(String ticker) throws IOException {
         OkHttpClient client = new OkHttpClient();
-
         Request request = new Request.Builder()
                 .url("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols="+ticker)
                 .get()
-                .addHeader("x-rapidapi-key", key)
-                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", "8d1894121cmsh1e839afb82a7fcdp196250jsn3f32627c72f4")
+                .addHeader("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
                 .build();
-        return client.newCall(request).execute();
-    }
-
-    public static void main(String[] args) throws IOException {
-        Response response = getCurrentResponse("AAPL");
-        System.out.print(response.body().string());
+        Response response = client.newCall(request).execute();
+        JsonArray address = new JsonParser().parse(response.body().string())
+                .getAsJsonObject().get("quoteResponse")
+                .getAsJsonObject().getAsJsonArray("result");
+        var stock = address.get(0);
+        var price = stock.getAsJsonObject().get("regularMarketPrice");
+        return price.getAsDouble();
     }
 }
