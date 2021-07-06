9ba5bc2805e5164c968c3387f2b61238c30720e3
caspari-svg <holguinsebastian9@gmail.com>
2021-07-06 17:10:53 -0400

    Cleared key/host from code. Returned correctly formatted decimal and added exception in case of unknown stock price. Further testing required for edge cases.

--- src/main/java/com/codediff/stock/StockRetriever.java
+++ src/main/java/com/codediff/stock/StockRetriever.java
@@ -4,23 +4,32 @@ import com.google.gson.*;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-import java.io.IOException;
 
 public class StockRetriever {
-    public static double getCurrentResponse(String ticker) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols="+ticker)
-                .get()
-                .addHeader("x-rapidapi-key", "8d1894121cmsh1e839afb82a7fcdp196250jsn3f32627c72f4")
-                .addHeader("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                .build();
-        Response response = client.newCall(request).execute();
-        JsonArray address = new JsonParser().parse(response.body().string())
-                .getAsJsonObject().get("quoteResponse")
-                .getAsJsonObject().getAsJsonArray("result");
-        var stock = address.get(0);
-        var price = stock.getAsJsonObject().get("regularMarketPrice");
-        return price.getAsDouble();
+    public static double getCurrentResponse(String ticker) {
+        /*
+        IMPORTANT! It seems the API doesn't recognize '.' in a stock. For example,
+        Berkshire Hathaway is known as BRK.A, but the API has it listed as BRK-A.
+        Not sure if this applies to every stock.
+         */
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=" + ticker)
+                    .get()
+                    .addHeader("x-rapidapi-key", System.getenv("api_key"))
+                    .addHeader("x-rapidapi-host", System.getenv("api_host"))
+                    .build();
+            Response response = client.newCall(request).execute();
+            JsonArray address = new JsonParser().parse(response.body().string())
+                    .getAsJsonObject().get("quoteResponse")
+                    .getAsJsonObject().getAsJsonArray("result");
+            var stock = address.get(0);
+            var price = stock.getAsJsonObject().get("regularMarketPrice");
+            String formatter = String.format("%.2f", price.getAsDouble());
+            return Double.parseDouble(formatter);
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
