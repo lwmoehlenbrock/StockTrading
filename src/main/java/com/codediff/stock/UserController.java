42ad480a102a133691bc6d9a18bbaba94520ca8d
Lucas <lwmoehlenbrock@ucdavis.edu>
2021-07-06 12:42:13 -0500

    Refactored StockController into StockController and UserController

--- src/main/java/com/codediff/stock/UserController.java
+++ src/main/java/com/codediff/stock/UserController.java
@@ -71,4 +71,8 @@ public class UserController {
         return new ResponseEntity<String>("Success! New balance: " + currentUser.getCash(), HttpStatus.ACCEPTED);
     }
 
+    @GetMapping("/balance")
+    public double getBalance() throws IOException{
+        return currentUser.getStockBalance();
+    }
 }
